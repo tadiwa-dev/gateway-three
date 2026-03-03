5b4030cd341deed25f9bf483efaae0fe14c51796
Tadiwa Matewe <tadiwahmatewe@gmail.com>
2026-03-03 12:56:25 +0200

    feat: animate hero component integration

--- app/(public-pages)/page.jsx
+++ app/(public-pages)/page.jsx
@@ -1,5 +1,6 @@
 import FaqSection from '@/sections/faq-section';
 import HeroSection from '@/sections/hero-section';
+import { HeroDemo } from '@/components/blocks/animated-hero';
 import Newsletter from '@/sections/newsletter';
 import FeaturesSection from '@/sections/features-section';
 import PersonasSection from '@/sections/personas-section';
@@ -8,6 +9,7 @@ export default function Page() {
     return (
         <main className='px-4'>
             <HeroSection />
+            <HeroDemo />
             <FeaturesSection />
             <PersonasSection />
             <FaqSection />
