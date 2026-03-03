ac9a2114d619d43fee95325e3103503a53118d08
Tadiwa Matewe <tadiwahmatewe@gmail.com>
2026-03-03 12:31:16 +0200

    refactor: replace tabler-icons with lucide-react

--- components/ui/feature-section-with-hover-effects.jsx
+++ components/ui/feature-section-with-hover-effects.jsx
@@ -2,15 +2,15 @@
 
 import { cn } from "@/lib/utils";
 import {
-    IconAdjustmentsBolt,
-    IconCloud,
-    IconCurrencyDollar,
-    IconEaseInOut,
-    IconHeart,
-    IconHelp,
-    IconRouteAltLeft,
-    IconTerminal2,
-} from "@tabler/icons-react";
+    Terminal,
+    MousePointerClick,
+    DollarSign,
+    Cloud,
+    Network,
+    HelpCircle,
+    Settings,
+    Heart,
+} from "lucide-react";
 
 export function FeaturesSectionWithHoverEffects() {
     const features = [
@@ -18,46 +18,46 @@ export function FeaturesSectionWithHoverEffects() {
             title: "Built for developers",
             description:
                 "Built for engineers, developers, dreamers, thinkers and doers.",
-            icon: <IconTerminal2 />,
+            icon: <Terminal />,
         },
         {
             title: "Ease of use",
             description:
                 "It's as easy as using an Apple, and as expensive as buying one.",
-            icon: <IconEaseInOut />,
+            icon: <MousePointerClick />,
         },
         {
             title: "Pricing like no other",
             description:
                 "Our prices are best in the market. No cap, no lock, no credit card required.",
-            icon: <IconCurrencyDollar />,
+            icon: <DollarSign />,
         },
         {
             title: "100% Uptime guarantee",
             description: "We just cannot be taken down by anyone.",
-            icon: <IconCloud />,
+            icon: <Cloud />,
         },
         {
             title: "Multi-tenant Architecture",
             description: "You can simply share passwords instead of buying new seats",
-            icon: <IconRouteAltLeft />,
+            icon: <Network />,
         },
         {
             title: "24/7 Customer Support",
             description:
                 "We are available a 100% of the time. Atleast our AI Agents are.",
-            icon: <IconHelp />,
+            icon: <HelpCircle />,
         },
         {
             title: "Money back guarantee",
             description:
                 "If you donot like EveryAI, we will convince you to like us.",
-            icon: <IconAdjustmentsBolt />,
+            icon: <Settings />,
         },
         {
             title: "And everything else",
             description: "I just ran out of copy ideas. Accept my sincere apologies",
-            icon: <IconHeart />,
+            icon: <Heart />,
         },
     ];
 
